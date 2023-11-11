0ef0f5621553bdff2fab58118cf2abe78d5ce428
DarthStorm <darthstorm08@gmail.com>
2023-11-11 10:28:40 +0800

    r]

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 allhref = document.getElementsByClassName("href");
 
-for (let i = 0; i < allhref.length; i++) {
-    allhref[i].addEventListener("onclick",function (e) {
-        window.location.href = allhref[i].href;
-    })
-    
+window.onload = function() {
+    for (let i = 0; i < allhref.length; i++) {
+        allhref[i].onclick = function (e) {
+            href = allhref[i].attributes["href"].value;
+            window.location.href = href;
+        }
+        console.log(allhref[i]);
+    }
 }
